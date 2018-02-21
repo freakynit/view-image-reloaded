1003ab1d86f83911fd1260817f38ce4d0149172d
nitin <nitin@webklipper.com>
2018-02-21 12:31:34 +0530

    changes

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 var s = document.createElement("script");
 s.type = "text/javascript";
-s.src = "https://cdn.rawgit.com/freakynit/view-image-reloaded/master/dynload.js";
+s.src = "https://rawgit.com/freakynit/view-image-reloaded/master/dynload.js";
 s.id = "vir-dynloaded";
 s.async = false;
 s.defer = false;
